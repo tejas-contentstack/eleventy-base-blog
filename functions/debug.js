35434aebc49e5cf11f5e795becdc37a74a310326
Kshateesh <kshateesh007@gmail.com>
2023-10-18 17:00:18 +0530

    Update debug.js

--- functions/debug.js
+++ functions/debug.js
@@ -1,15 +1,5 @@
-import { lambdaHandler } from "../index.js";
+
 export default function handler(req, res) {
-    console.log("in cloud function handler!!!!!!!!!!!!!!!!!!!");
-    console.log("req.query ", req.query);
-    console.log("req.body ", req.body);
-    var event = {
-        queryStringParameters: req.query,
-        body: req.body,
-    };
-    console.log("--------------------");
-    console.log(event);
-    console.log("*****************lambdaHandler***************");
     console.log(lambdaHandler);
     console.log("calling handler");
     res.send("Hello");
