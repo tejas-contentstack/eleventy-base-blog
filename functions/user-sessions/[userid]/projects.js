b515854ad41af79851b08cf19caa34fe83b56509
kshateesh <kshateesh007@gmail.com>
2024-05-27 11:37:54 +0530

    add new user session dir

---
+++ functions/user-sessions/[userid]/projects.js
@@ -0,0 +1,8 @@
+export default function handler(req, res) {
+	console.log({ params: req.params });
+	console.log({ query: req.query });
+	console.log({ body: req.body });
+	res
+		.status(200)
+		.send({ message: `This is user-sessions projects`, query: req.query });
+}
