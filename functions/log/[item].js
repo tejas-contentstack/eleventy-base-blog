636b986e99979e64271dd1acf0a0329e141a0e83
Kshateesh <kshateesh007@gmail.com>
2024-08-16 16:39:34 +0530

    Update [item].js

--- functions/log/[item].js
+++ functions/log/[item].js
@@ -3,5 +3,5 @@ export default function handler(req, response) {
   console.log({ query: req.query });
   console.log({ params: req.params });
 
-  res.status(200).send(`This params ${JSON.stringify(req.params.item)} and this is query ${JSON.stringify(req.query)}`);
+  res.status(200).send("Test");
 }
