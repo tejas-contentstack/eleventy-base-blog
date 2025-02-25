93edd9e71ad6245a224bb2b09752e8936fe6a7e5
tejas-contentstack <132910969+tejas-contentstack@users.noreply.github.com>
2025-02-25 18:56:35 +0530

    Update sleep.js

--- functions/sleep.js
+++ functions/sleep.js
@@ -1,10 +1,11 @@
 export default async function handler(req, res) {
 	const sleepTime = 3 * 60* 1000;
-	setInterval(() => {
-	console.log("Printing something every 10 seconds");
+	const interval = setInterval(() => {
+    console.log('printing every 10 seconds');
 	}, 10_000);
   await sleep(sleepTime)
   console.log('Waited for 3 minutes!!!');
+	clearInterval(interval);
 	res.status(200).send("sent!");
 }
 
